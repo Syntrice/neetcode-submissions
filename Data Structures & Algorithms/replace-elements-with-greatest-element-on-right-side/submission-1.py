04396ba3fd9976faf567248492b348fa34d14088
Sam Maughan <syntrice@protonmail.com>
2026-08-10 10:58:10 +0100

    Add: replace-elements-with-greatest-element-on-right-side - submission-1

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        # Variation of "prefix sum" except in reverse and computing maximum
+        n = len(arr)
+        result = [-1] * n # prebuild array for efficient insertion
+        for i in range(n - 2, -1, -1): # traverse backwards starting second to last
+            # the item to the right in `result` stores then maximum of everything
+            # previously examined while the item to the right in `arr` is the new
+            # we are examining, so it's the max of either of these
+            result[i] = max(result[i + 1], arr[i + 1]) 
+
+        return result
+
+# Official solution slightly clearer:
+# class Solution:
+#     def replaceElements(self, arr: List[int]) -> List[int]:
+#         n = len(arr)
+#         ans = [0] * n
+#         rightMax = -1
+#         for i in range(n - 1, -1, -1):
+#             ans[i] = rightMax
+#             rightMax = max(arr[i], rightMax)
+#         return ans
